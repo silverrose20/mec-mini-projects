09e9efd3efe083ce19390a6ddc4e0ed65cb440b8
KyokoS <suenaga80@hotmail.com>
2022-06-19 17:04:52 -0700

    mini-project 5.5.6 dataset created

---
+++ mec-5.5.4-webscraping-project/toscrape_xpath.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'toscrape-xpath'
+
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                'text': quote.xpath('.//span[@class="text"]/text()').get(),
+                'author': quote.xpath('.//span/small[@class="author"]/text()').get(),
+                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall(),
+            }
+
+        next_page = response.xpath('//li[@class="next"]/a/@href').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
